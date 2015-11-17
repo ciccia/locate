695e3e3e851a26e17230c88878e4af4af8380bfe
Cristian Cialli <cristian.cialli@gmail.com>
2015-11-17 22:33:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# locate
+
+## Tests
+You can run tests with mocha
+
+### Installation
+
+```sh
+$ npm install --global mocha
+```
+### Run
+
+```sh
+$ npm test
+```
+or
+
+```sh
+$ mocha
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    let locate = function(values, lookingFor) {
+        if (values.indexOf(lookingFor) != -1) {
+            return true
+        }
+
+        return values.some(function(value) {
+            return Array.isArray(value) && locate(value, lookingFor);
+        })
+    }
+
+    module.exports = locate;
+
+})()
